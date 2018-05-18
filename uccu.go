8f87f5283680f8594d27ab77031ed385c00af257
芮智鹏 <ruizhipeng001@gmail.com>
2018-05-18 20:41:16 +0800

    uccuv0.6

--- uccu.go
+++ uccu.go
@@ -43,13 +43,14 @@ func FromCmd(CmdArgs WatchOnit) WatchOnit {
 	var proc, contact, arg, num string
 
 	//不确定是不是应该写一个-h或者用别的方式来设计.... 明天问
-	flag.StringVar(&proc, "cmd", "", "需要监控的程序")
+	flag.StringVar(&proc, "bin", "", "需要监控的程序")
 	flag.StringVar(&arg, "args", "", "程序启动的参数")
 	flag.StringVar(&contact, "tel", "", "告警联系人电话,多个时用逗号分开")
 	flag.StringVar(&CmdArgs.APIADDR, "api", "", "短信api地址")
 	flag.Int64Var(&CmdArgs.HeartBeat, "hb", 60, "心跳频率,单位:秒")
 
 	CmdArgs.Proc = proc
+	CmdArgs.Args = append(CmdArgs.Args, proc)
 	CmdArgs.Args = append(CmdArgs.Args, arg)
 
 	for _, num = range strings.Split(contact, ",") {
@@ -70,13 +71,6 @@ func MsgOrNot(CmdArgs WatchOnit) string {
 		postmsg.Content = fmt.Sprintf("%s又挂啦,修不了啦", CmdArgs.Proc)
 
 		bytesData, _ := json.Marshal(postmsg)
-		/*
-			if err != nil {
-				fmt.Println(err.Error())
-				log.Error(err)
-				return string("")
-			}
-		*/
 		reader := bytes.NewReader(bytesData)
 		url := fmt.Sprintf("%s%s", CmdArgs.APIADDR, CmdArgs.Proc)
 		req, err := http.NewRequest("POST", url, reader)
@@ -93,11 +87,11 @@ func MsgOrNot(CmdArgs WatchOnit) string {
 		e := json.Unmarshal(body, &respmsg)
 		if e != nil {
 			fmt.Println(e.Error())
-			os.Exit(1)
+			return "短信接口返回异常"
 		}
 		return respmsg.Msg
 	}
-	return fmt.Sprint("没写联系人，我也不知道联系谁")
+	return "没写联系人，我也不知道联系谁"
 }
 
 func uccu(c WatchOnit) {
